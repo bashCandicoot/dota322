6ca677ed747c1b1b0c7e615059910b2e7b19a584
steve <bashcandicoot@gmail.com>
2017-12-19 17:26:35 +0000

    using express for routing

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
-var http = require('http'),
-    fs = require('fs')
+const express = require('express')
+const app = express()
 
-fs.readFile('./index.html', function (err, html) {
-    http.createServer(function(request, response) { 
-        response.writeHeader(200, {"Content-Type": "text/html"});
-        response.write(html);
-        response.end();
-}).listen(8080, 'localhost')
-})
+app.use('/', express.static('.'))
+app.listen(8080)
