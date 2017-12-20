68fd5d546df341e29c28155c2b0f7bcd819e664b
steve <bashcandicoot@gmail.com>
2017-12-20 20:16:58 +0000

    move port and server logic into start.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express = require('express')
+const app = express()
+
 require('dotenv').config({ path: '.env'})
 
-const app = express()
 app.use('/', express.static('.'))
 
-app.set('port', process.env.port)
-const server = app.listen(app.get('port'))
+module.exports = app;
