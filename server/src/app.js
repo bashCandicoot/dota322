258a7f0df64732f218e771ea4d550d1c4f350a89
steve <bashcandicoot@gmail.com>
2018-01-23 12:48:54 +0000

    added body-parser, cors and morgan

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,20 @@
 require('dotenv').config({ path: '.env' });
 
 const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
 
 const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.use('/', express.static('../../client'));
-
-// app.get('/', (req, res) => {
-//   res.send({
-//     message: 'hello',
-//   });
-// });
+// app.use('/', express.static('../../client'));
+app.get('/', (req, res) => {
+  res.send({
+    message: 'hellos',
+  });
+});
 
 module.exports = app;
